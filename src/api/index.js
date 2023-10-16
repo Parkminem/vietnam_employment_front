0857d129bed90334fb1fdd77b23df2afd3ba20ef
Minhyuk Park <comon23463@gmail.com>
2023-10-16 10:42:43 +0900

    fix: api url

--- src/api/index.js
+++ src/api/index.js
@@ -1,3 +1,3 @@
 // export const baseUrl = "http://64.110.86.131";
-// export const baseUrl = "http://ideaconcert.art";
-export const baseUrl = "https://test.ideaconcert.art";
+export const baseUrl = "https://ideaconcert.art";
+// export const baseUrl = "https://test.ideaconcert.art";
